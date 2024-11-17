a33aee0cca32e4823cb3662ce4fb51de88135f6c
BlueBerrySoda03 <chstephens.2003@gmail.com>
2024-11-16 21:11:45 -0500

    Add files via upload

--- main/java/com/blueberrysoda/mysticalagriberry/items/Amalgam.java
+++ main/java/com/blueberrysoda/mysticalagriberry/items/Amalgam.java
@@ -1,10 +1,15 @@
 package com.blueberrysoda.mysticalagriberry.items;
 
 import com.blueberrysoda.mysticalagriberry.items.base.ItemAmalgam;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class Amalgam {
@@ -82,5 +87,32 @@ public class Amalgam {
         event.getRegistry().registerAll(ENDER_DRAGON_AMALGAM);
         event.getRegistry().registerAll(WITHER_AMALGAM);
         //event.getRegistry().registerAll();
+    }
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public static void registerItemModels(ModelRegistryEvent event) {
+        ModelLoader.setCustomModelResourceLocation(ZOMBIE_AMALGAM, 0, new ModelResourceLocation(ZOMBIE_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(PIG_AMALGAM, 0, new ModelResourceLocation(PIG_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(CHICKEN_AMALGAM, 0, new ModelResourceLocation(CHICKEN_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(COW_AMALGAM, 0, new ModelResourceLocation(COW_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(SHEEP_AMALGAM, 0, new ModelResourceLocation(SHEEP_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(SLIME_AMALGAM, 0, new ModelResourceLocation(SLIME_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(SKELETON_AMALGAM, 0, new ModelResourceLocation(SKELETON_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(CREEPER_AMALGAM, 0, new ModelResourceLocation(CREEPER_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(SPIDER_AMALGAM, 0, new ModelResourceLocation(SPIDER_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(RABBIT_AMALGAM, 0, new ModelResourceLocation(RABBIT_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(GUARDIAN_AMALGAM, 0, new ModelResourceLocation(GUARDIAN_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(BLAZE_AMALGAM, 0, new ModelResourceLocation(BLAZE_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(GHAST_AMALGAM, 0, new ModelResourceLocation(GHAST_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(ENDERMAN_AMALGAM, 0, new ModelResourceLocation(ENDERMAN_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(WITHER_SKELETON_AMALGAM, 0, new ModelResourceLocation(WITHER_SKELETON_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(BLIZZ_AMALGAM, 0, new ModelResourceLocation(BLIZZ_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(BLITZ_AMALGAM, 0, new ModelResourceLocation(BLITZ_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(BASALZ_AMALGAM, 0, new ModelResourceLocation(BASALZ_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(WITCH_AMALGAM, 0, new ModelResourceLocation(WITCH_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(ENDER_DRAGON_AMALGAM, 0, new ModelResourceLocation(ENDER_DRAGON_AMALGAM.delegate.name(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(WITHER_AMALGAM, 0, new ModelResourceLocation(WITHER_AMALGAM.delegate.name(), "inventory"));
+        //ModelLoader.setCustomModelResourceLocation(, 0, new ModelResourceLocation(.delegate.name(), "inventory"));
     }
 }
